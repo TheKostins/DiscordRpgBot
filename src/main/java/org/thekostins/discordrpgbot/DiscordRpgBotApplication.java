299488b5d2af317c519e1e03524c51c78b1b778d
Konstantin <anisimovkosta19@gmail.com>
2021-10-11 12:07:39 +0300

    Initial Commit

---
+++ src/main/java/org/thekostins/discordrpgbot/DiscordRpgBotApplication.java
@@ -0,0 +1,13 @@
+package org.thekostins.discordrpgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordRpgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordRpgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thekostins/discordrpgbot/DiscordRpgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thekostins.discordrpgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordRpgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
